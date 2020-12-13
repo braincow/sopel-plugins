7c1801b9cdf6ba992877eb4f1efc3157c1e49955
Antti Peltonen <antti.peltonen@iki.fi>
2020-12-13 13:41:10 +0200

    added  custom exception for when API endpoint is defunct

--- ruuvitag.py
+++ ruuvitag.py
@@ -6,6 +6,10 @@ import google.auth.transport.requests
 from google.oauth2.service_account import IDTokenCredentials
 
 
+class RuuvitagError(Exception):
+    pass
+
+
 class RuuvitagSection(StaticSection):
     sa_json = ValidatedAttribute('sa_json', str)
     endpoint = ValidatedAttribute('endpoint', str)
@@ -17,8 +21,12 @@ def setup(bot):
 
 def configure(config):
     config.define_section('ruuvitag', RuuvitagSection, validate=False)
-    config.ruuvitag.configure_setting('sa_json', 'Where is the GCP service account key file located at (aka filename)?')
-    config.ruuvitag.configure_setting('endpoint', 'What is the GCP cloud function endpoint URL to call?')
+    config.ruuvitag.configure_setting(
+            'sa_json',
+            'Where is the GCP service account key file located at (aka filename)?')
+    config.ruuvitag.configure_setting(
+            'endpoint',
+            'What is the GCP cloud function endpoint URL to call?')
 
 
 def invoke_endpoint(url, id_token):
@@ -27,10 +35,8 @@ def invoke_endpoint(url, id_token):
     r = requests.get(url, headers=headers)
 
     if r.status_code != 200:
-        print('Calling endpoint failed')
-        print('HTTP Status Code:', r.status_code)
-        print(r.content)
-        return None
+        raise RuuvitagError(
+                "Calling API endpoint failed with HTTP status code: {}".format(r.status_code))
 
     return r.content.decode('utf-8')
 
@@ -58,6 +64,10 @@ def ruuvitag(bot, trigger):
     tags = dict()
     for tag in json.loads(response):
         tags[tag["name"]] = tag["data"]
+
+    if len(tags) == 0:
+        bot.say("No latest tag information available.")
+        return
 
     if trigger.group(2) is not None:
         if trigger.group(2) not in tags:
